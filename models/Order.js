b7266cfe0490c80647a8ec1362c82929dc48ff7a
Your Name <your@email.com>
2025-11-09 19:19:55 +0000

    Resolve remaining merge conflicts

--- models/Order.js
+++ models/Order.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
-    lessonIDs: { type: [String], required: true }, // array of lesson _id strings
-    numberOfSpaces: { type: Number, required: true }
+    name:         { type: String, required: true },
+    phoneNumber:  { type: String, required: true },
+    lessonIDs:    { type: [String], required: true }, // array of lesson _id strings
+    numberOfSpaces:{ type: Number, required: true }
   },
   { timestamps: true }
 );
